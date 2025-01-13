8c59a5c41c31cd495fb3a65a099c6649194bf86b
MOHAMMED MUSTHAKEEM <98821339+git-mohammedmusthakeem@users.noreply.github.com>
2025-01-13 23:34:54 +0530

    Add files via upload

---
+++ 377. Combination Sum IV.java
@@ -0,0 +1,12 @@
+class Solution {
+        public int combinationSum4(int[] nums, int target) {
+            int[] dp = new int[target + 1];
+            dp[0] = 1;
+            for(int i = 1; i <= target; i++){
+                for(int num : nums){
+                    dp[i] += i - num >= 0 ? dp[i - num]: 0;
+                }
+            }
+            return dp[target];
+        }
+    }
